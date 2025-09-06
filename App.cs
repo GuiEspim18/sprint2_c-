d0e3da0ff8c980bb6a157b9aa0a52470ba25e25b
guilherme.monteiro.espim@gmail.com <guilherme.monteiro.espim@gmail.com>
2025-09-06 17:48:07 -0300

    init project

---
+++ App.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
+
+namespace InvestYOU;
+
+public class App : Application
+{
+    public App()
+    {
+        MainPage = new ContentPage
+        {
+            BackgroundColor = Colors.White,
+            Content = new VerticalStackLayout
+            {
+                Padding = 30,
+                Children =
+                {
+                    new Label {
+                        Text = "InvestYOU sem XAML!",
+                        HorizontalOptions = LayoutOptions.Center
+                    },
+                    new Button
+                    {
+                        Text = "Clique aqui",
+                        Command = new Command(() => MainPage.DisplayAlert("Oi", "Funcionou!", "OK"))
+                    }
+                }
+            }
+        };
+    }
+}
